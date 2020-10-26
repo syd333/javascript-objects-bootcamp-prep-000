7718c9776e4ade5fbc1ef0cc871694465700b22e
sydney miranda <sydmiranda@icloud.com>
2020-10-26 21:56:31 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,7 +3,8 @@ var playlist = {
 }
 
 function updatePlaylist (playlist, artistName, songTitle) {
-return  playlist.artistName = songTitle
+playlist.artistName = songTitle
+return playlist
 }
 
 function removeFromPlaylist(playlist, artistName) {
